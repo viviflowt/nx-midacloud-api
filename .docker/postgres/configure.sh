beb539a7d7354e62d1cd7b6575199455ca411868
Viviane Florido <vivianeflowt@gmail.com>
2023-07-17 21:22:07 -0300

    update

--- .docker/postgres/configure.sh
+++ .docker/postgres/configure.sh
@@ -32,28 +32,12 @@ psql -v ON_ERROR_STOP=1 \
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
     -- create database
-    CREATE DATABASE identity;
-    GRANT ALL PRIVILEGES ON DATABASE identity TO $POSTGRES_USER;
-    ALTER DATABASE identity SET timezone TO 'America/Sao_Paulo';
-    ALTER DATABASE identity SET datestyle TO 'ISO, DMY';
-    ALTER DATABASE identity SET intervalstyle TO 'iso_8601';
-    ALTER DATABASE identity SET default_text_search_config TO 'pg_catalog.portuguese';
-
-    -- create database
-    CREATE DATABASE gateway;
-    GRANT ALL PRIVILEGES ON DATABASE gateway TO $POSTGRES_USER;
-    ALTER DATABASE gateway SET timezone TO 'America/Sao_Paulo';
-    ALTER DATABASE gateway SET datestyle TO 'ISO, DMY';
-    ALTER DATABASE gateway SET intervalstyle TO 'iso_8601';
-    ALTER DATABASE gateway SET default_text_search_config TO 'pg_catalog.portuguese';
-
-    -- create database
-    CREATE DATABASE notification;
-    GRANT ALL PRIVILEGES ON DATABASE notification TO $POSTGRES_USER;
-    ALTER DATABASE notification SET timezone TO 'America/Sao_Paulo';
-    ALTER DATABASE notification SET datestyle TO 'ISO, DMY';
-    ALTER DATABASE notification SET intervalstyle TO 'iso_8601';
-    ALTER DATABASE notification SET default_text_search_config TO 'pg_catalog.portuguese';
+    CREATE DATABASE account;
+    GRANT ALL PRIVILEGES ON DATABASE account TO $POSTGRES_USER;
+    ALTER DATABASE account SET timezone TO 'America/Sao_Paulo';
+    ALTER DATABASE account SET datestyle TO 'ISO, DMY';
+    ALTER DATABASE account SET intervalstyle TO 'iso_8601';
+    ALTER DATABASE account SET default_text_search_config TO 'pg_catalog.portuguese';
 
     -- create database
     CREATE DATABASE tenant;
@@ -62,6 +46,14 @@ psql -v ON_ERROR_STOP=1 \
     ALTER DATABASE tenant SET datestyle TO 'ISO, DMY';
     ALTER DATABASE tenant SET intervalstyle TO 'iso_8601';
     ALTER DATABASE tenant SET default_text_search_config TO 'pg_catalog.portuguese';
+
+    -- create database
+    CREATE DATABASE notification;
+    GRANT ALL PRIVILEGES ON DATABASE notification TO $POSTGRES_USER;
+    ALTER DATABASE notification SET timezone TO 'America/Sao_Paulo';
+    ALTER DATABASE notification SET datestyle TO 'ISO, DMY';
+    ALTER DATABASE notification SET intervalstyle TO 'iso_8601';
+    ALTER DATABASE notification SET default_text_search_config TO 'pg_catalog.portuguese';
 EOSQL
 
 echo -e "***** postgres setup complete *****"
